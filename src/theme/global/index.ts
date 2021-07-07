f198452c5a6ba2705b1faecaa0e38ec24b157fd3
Dawid Biernat <davd.biernat@gmail.com>
2021-07-07 20:12:43 +0200

    Add styled-components Typescript types. Add global styles

---
+++ src/theme/global/index.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+  *, *::before, *::after {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-size: 1.6rem;
+    background-color: white;
+  }
+
+`;
+
+export default GlobalStyle;
