420e303f8af4507817dcf2bad862c2d89cbb4ac5
Dawid Biernat <48552109+davdb@users.noreply.github.com>
2021-07-07 20:14:10 +0200

    Fea 1 (#3)
    * Add styled-components
    
    * Add styled-components Typescript types. Add global styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom";
 
 import App from "pages/_App";
 
+import { GlobalStyle } from "theme";
+
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
